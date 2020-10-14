16abea2edbfe37ea7168d4bf23ce511b0ba0890c
smth-0 <35414314+smth-0@users.noreply.github.com>
2020-10-14 21:11:02 +0600

    cleanup

--- main.py
+++ main.py
@@ -23,18 +23,7 @@ class MyClient(discord.Client):
 
         if message.content == 'ping':
             await message.channel.send('pong!')
-
-        if self.isWaitingForSlap and message.author.id == self.playerOne:
-            self.playerTwo = int(message.content[2:-1])
-            await self.battleCh.send("И сегодня на ринге боев без правил у нас <@" + str(self.playerOne) + "> and <@" + str(self.playerTwo) + ">!!!")
-            self.isGameRunning = True
-
-        if message.content == 'Да начнется битва!':
-            self.battleCh = message.channel
-            await self.battleCh.send('Сейчас все устрою!\nС кем будем махаться?')
-            self.isWaitingForSlap = True
-            self.playerOne = message.author.id
-
+            
 
 client = MyClient()
 client.run(tokenFile.token)
